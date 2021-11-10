f3238f6dadb324c9be2798a7701663b98c26cdad
Collins Lagat <cklagat@gmail.com>
2021-11-10 13:49:06 +0300

    feat: clean up and add check for non-existent user

--- app/controllers/spree/custom_user_auth_controller.rb
+++ app/controllers/spree/custom_user_auth_controller.rb
@@ -1,21 +1,19 @@
 class Spree::CustomUserAuthController < ActionController::API
-  # include Spree::Core::ControllerHelpers::Auth
-
   def create
     payload = params[:custom_user_auth]
+
     email = payload[:email]
     password = payload[:password]
 
     user = Spree::User.find_by(email: email)
 
-    # get_last_access_token = ->(user) { Spree::OauthAccessToken.active_for(user).where(expires_in: nil).last }
-    # create_access_token = ->(user) { Spree::OauthAccessToken.create!(resource_owner: user) }
-
-    # user = get_last_access_token.call(user) || create_access_token.call(user)
+    if user === nil
+      render json: {error: Spree.t(:authorization_failure)}
+      return
+    end
 
     sign_in(user, event: :authentication)
-    redirect_to spree.admin_path, notice: Spree.t(:account_updated)
-    # render json: spree.account_path
-    # render plain: defined?(spree)
+
+    redirect_to spree.admin_path
   end
 end
