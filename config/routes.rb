d36241e5a4794ce59c8000b7864e4ae9dc9b4887
Collins Lagat <cklagat@gmail.com>
2021-11-11 11:46:20 +0300

    fix: add devise scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Spree::Core::Engine.add_routes do
-  post 'ryanada/auth', controller: 'custom_user_auth', action: 'auto_login'
+  devise_scope :spree_user do
+    post 'ryanada/auth', controller: 'custom_user_auth', action: 'auto_login'
+  end
 end
