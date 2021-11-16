c9e2c6ae0946ead1ec6f7105f405509e38f67e4b
Collins Lagat <cklagat@gmail.com>
2021-11-16 12:50:44 +0300

    feat: use GET instead of POST

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  post 'admin/auto_login', controller: 'olitt/users', action: 'auto_login'
+  get 'admin/auto_login', controller: 'olitt/users', action: 'auto_login'
 end
