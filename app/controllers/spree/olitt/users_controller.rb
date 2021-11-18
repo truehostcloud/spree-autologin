5e916da85e9b6f7d8e201b2bed9d1f3a13c24bd7
Stephen Gachoka <stephengachoka57@gmail.com>
2021-11-18 16:05:34 +0300

    contoller params

--- app/controllers/spree/olitt/users_controller.rb
+++ app/controllers/spree/olitt/users_controller.rb
@@ -34,7 +34,7 @@ module Spree
       end
 
       def activate_vendor(vendor_id)
-        ::Spree::Vendor.update(vendor_id: vendor_id, state: 'active')
+        Spree::Admin::VendorsController.update(vendor_id: vendor_id, state: 'active')
       end
 
       # this is a boolean method 🤪
