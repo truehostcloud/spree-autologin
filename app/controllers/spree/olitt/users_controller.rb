c66b1fcbc96fbcde348a228179d5bdb2105905b8
William Mwai <willymwaid92@gmail.com>
2022-02-23 11:02:16 +0300

    chore: use base64 encoded email:password

--- app/controllers/spree/olitt/users_controller.rb
+++ app/controllers/spree/olitt/users_controller.rb
@@ -2,7 +2,7 @@ module Spree
   module Olitt
     class UsersController < Spree::Api::V2::BaseController
       def auto_login # rubocop:disable Metrics/AbcSize
-        email, password = login_details
+        email, password, next_path = login_details
         user = Spree::User.find_by(email: email)
         vendor = ::Spree::Vendor.active.find_by(slug: email)
 
@@ -12,19 +12,29 @@ module Spree
 
         activate_vendor
         sign_in(user, event: :authentication)
-        redirect_to spree.admin_path
+        redirect_to next_path
       end
 
       private
 
       def login_details
+        basic_auth = params[:basic_auth]
+        next_path = params[:next] || spree.admin_path
+        # backward compatibility for old api
         email = params[:email]
         password = params[:password]
+        raise ActionController::ParameterMissing, :basic_auth if basic_auth.nil? && (email.nil? || password.nil?)
+        if basic_auth
+          auth_string = Base64.decode64(basic_auth)
+          # convert auth_string to utf-8
+          auth_string = auth_string.force_encoding('utf-8')
+          email, password = auth_string.split(':')
+        end
 
         raise ActionController::ParameterMissing, :email if email.nil?
         raise ActionController::ParameterMissing, :password if password.nil?
 
-        [email, password]
+        [email, password, next_path]
       end
 
       def create_vendor(email)
