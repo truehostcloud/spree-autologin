69350e36744cc25ea341d8307b4e424a61b07172
kipsang <kipsang1998@gmail.com>
2026-03-25 16:49:48 +0300

    refactor: enhance auto-login functionality and update gemspec for version 0.1.0

--- app/controllers/spree/olitt/users_controller.rb
+++ app/controllers/spree/olitt/users_controller.rb
@@ -1,58 +1,93 @@
+require 'base64'
+require 'uri'
+
 module Spree
   module Olitt
-    class UsersController < Spree::Api::V2::BaseController
-      def auto_login # rubocop:disable Metrics/AbcSize
+    class UsersController < Spree::BaseController
+      def auto_login
         email, password, next_path = login_details
-        user = Spree::User.find_by(email: email)
-        vendor = ::Spree::Vendor.active.find_by(slug: email)
 
-        vendor = create_vendor(email) if vendor_email_exist?(vendor)
-        user = create_user(email, password, vendor.id) if user_email_exists?(user)
-        raise CanCan::AccessDenied unless user.valid_password?(password)
+        user = nil
+        vendor = nil
 
-        vendor.update(state: 'active')
+        Spree.user_class.transaction do
+          vendor = find_or_create_vendor(email)
+          user = find_or_create_user(email, password)
+
+          raise CanCan::AccessDenied unless user.valid_password?(password)
+
+          assign_vendor_role(user, vendor)
+          activate_vendor(vendor)
+        end
+
         sign_in(user, event: :authentication)
-        redirect_to next_path
+        redirect_to next_path, allow_other_host: false
       end
 
       private
 
       def login_details
         basic_auth = params[:basic_auth]
-        next_path = params[:next] || spree.admin_path
-        # backward compatibility for old api
+        next_path = sanitize_next_path(params[:next]) || spree.admin_dashboard_path
         email = params[:email]
         password = params[:password]
+
         raise ActionController::ParameterMissing, :basic_auth if basic_auth.nil? && (email.nil? || password.nil?)
+
         if basic_auth
-          auth_string = Base64.decode64(basic_auth)
-          # convert auth_string to utf-8
-          auth_string = auth_string.force_encoding('utf-8')
-          email, password = auth_string.split(':')
+          auth_string = Base64.strict_decode64(basic_auth.to_s).force_encoding('utf-8')
+          email, password = auth_string.split(':', 2)
         end
 
         raise ActionController::ParameterMissing, :email if email.nil?
         raise ActionController::ParameterMissing, :password if password.nil?
 
-        [email, password, next_path]
+        [email.to_s.strip.downcase, password, next_path]
+      rescue ArgumentError
+        raise ActionController::ParameterMissing, :basic_auth
       end
 
-      def create_vendor(email)
-        ::Spree::Vendor.create(name: email, notification_email: email)
+      def find_or_create_vendor(email)
+        ::Spree::Vendor.find_by(notification_email: email) ||
+          ::Spree::Vendor.find_by(name: email) ||
+          ::Spree::Vendor.create!(
+            name: email,
+            notification_email: email,
+            contact_person_email: email,
+            billing_email: email
+          )
       end
 
-      # this is a boolean method
-      def vendor_email_exist?(vendor)
-        vendor.nil?
+      def find_or_create_user(email, password)
+        Spree.user_class.find_by(email: email) || Spree.user_class.create!(email: email, password: password)
       end
 
-      # this is also a boolean method
-      def user_email_exists?(user)
-        user.nil?
+      def assign_vendor_role(user, vendor)
+        vendor_role_name = defined?(Spree::Vendor::DEFAULT_VENDOR_ROLE) ? Spree::Vendor::DEFAULT_VENDOR_ROLE : 'vendor'
+        vendor_role = Spree::Role.find_or_create_by!(name: vendor_role_name)
+
+        Spree::RoleUser.find_or_create_by!(
+          user: user,
+          role: vendor_role,
+          resource: vendor
+        )
       end
 
-      def create_user(email, password, vendor_id)
-        Spree::User.create(email: email, password: password, spree_role_ids: [2], vendor_ids: [vendor_id])
+      def activate_vendor(vendor)
+        return if %w[active approved].include?(vendor.state)
+
+        vendor.start_onboarding! if vendor.respond_to?(:start_onboarding!) && vendor.state == 'invited'
+        vendor.approve! if vendor.respond_to?(:approve!) && !%w[active approved].include?(vendor.state)
+      end
+
+      def sanitize_next_path(next_path)
+        return if next_path.blank?
+        return next_path if next_path.start_with?('/') && !next_path.start_with?('//')
+
+        uri = URI.parse(next_path)
+        uri.path if uri.host.nil? && uri.scheme.nil? && uri.path.present?
+      rescue URI::InvalidURIError
+        nil
       end
     end
   end
